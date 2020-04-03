5480c72ff8cbce4f7c282f0dd3c3109caf03783e
Liam Ma <liam.q.ma@gmail.com>
2020-04-03 21:53:28 +1100

    Remove unnecessary CSS

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -20,8 +20,6 @@ const NameButton = styled.button`
     word-break: break-all;
     padding: 15px 15px 15px 50px;
     line-height: 1.2;
-    transition: color 0.4s;
-    position: relative;
     background-image: url("data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%23ededed%22%20stroke-width%3D%223%22/%3E%3C/svg%3E");
     background-repeat: no-repeat;
     background-position: center left;
@@ -49,34 +47,15 @@ const Table = styled.table`
     margin: 0 auto;
     width: 100%;
     text-align: left;
-    border-spacing: 0;
     border-collapse: collapse;
     tr:first-child th {
         border-bottom: 1px solid #65cec8;
         padding: 15px 15px;
-        background-color: #ffffff44;
-        font-size: 1.1em;
     }
     tr td,
     tr:not(:first-child) th {
         padding: 12px 20px;
-        position: relative;
         font-weight: normal;
-    }
-    tr:not(:first-child) th {
-        transition: all 1s ease-out;
-    }
-
-    .tr td::after,
-    .tr:not(first-child) th::after {
-        width: 0;
-        height: 2px;
-        background-color: #ffffff22;
-        content: " ";
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        transition: width 0.1s ease-out 0.2s, height 0.2s ease 0.4s;
     }
 `;
 
