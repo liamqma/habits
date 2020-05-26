3a77e96cc8a715a9db0f28d700b3b9a30f60e260
Liam Ma <liam.q.ma@gmail.com>
2020-05-26 14:00:07 +1000

    Integration test delete item

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { render, wait, fireEvent } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { getAll, add, update } from "../../repository/firestore";
+import { getAll, add, update, remove } from "../../repository/firestore";
+import window from "global/window";
 import useAuth from "../../hooks/use-auth";
 import App from "./App";
 import { buildItem } from "../../../test/utils/generate";
 
-const user = {
+const mockedUser = {
     uid: "User A",
 };
 
@@ -20,11 +21,44 @@ beforeEach(async () => {
     (getAll as jest.Mock).mockReset();
 });
 
+test("delete item", async () => {
+    const fakeItem = buildItem();
+    (useAuth as jest.Mock).mockReturnValue({
+        user: mockedUser,
+    });
+    (getAll as jest.Mock).mockReturnValueOnce(Promise.resolve([fakeItem]));
+    (remove as jest.Mock).mockReturnValueOnce(Promise.resolve(null));
+    (window.confirm as jest.Mock).mockReturnValueOnce(true);
+
+    const { getByDisplayValue, queryByText, container, getByTestId } = render(
+        <App />
+    );
+
+    await wait();
+
+    const link = container.querySelector(`[href="/edit/${fakeItem.id}"]`);
+    if (link === null) {
+        throw new Error(`unable to find the link "/edit/${fakeItem.id}"`);
+    }
+    userEvent.click(link);
+
+    await wait(() => {
+        getByDisplayValue(fakeItem.name);
+    });
+
+    userEvent.click(getByTestId("remove"));
+    expect(remove).toBeCalledWith(fakeItem.id);
+    expect(remove).toBeCalledTimes(1);
+
+    await wait();
+    expect(queryByText(fakeItem.name)).not.toBeInTheDocument();
+});
+
 test("update item", async () => {
     const fakeItem = buildItem();
     const fakeItem2 = buildItem();
     (useAuth as jest.Mock).mockReturnValue({
-        user,
+        user: mockedUser,
     });
     (getAll as jest.Mock).mockReturnValueOnce(Promise.resolve([fakeItem]));
     (update as jest.Mock).mockResolvedValueOnce(null);
@@ -52,14 +86,10 @@ test("update item", async () => {
     expect(update).toBeCalledTimes(1);
 });
 
-beforeEach(() => {
-    (add as jest.Mock).mockReset();
-});
-
 test("add item", async () => {
     const fakeItem = buildItem();
     (useAuth as jest.Mock).mockReturnValue({
-        user,
+        user: mockedUser,
     });
     (getAll as jest.Mock).mockReturnValueOnce(Promise.resolve([]));
 
@@ -72,7 +102,7 @@ test("add item", async () => {
     userEvent.type(input, fakeItem.name);
     fireEvent.submit(getByRole("form"));
 
-    expect(add).toBeCalledWith(user.uid, fakeItem.name);
+    expect(add).toBeCalledWith(mockedUser.uid, fakeItem.name);
     expect(add).toBeCalledTimes(1);
 
     await wait(() => {
@@ -93,7 +123,7 @@ test("login and render list, add item, update item", async () => {
     getByText(/sign in with/i);
 
     (useAuth as jest.Mock).mockReturnValue({
-        user,
+        user: mockedUser,
     });
 
     (getAll as jest.Mock).mockReturnValueOnce(Promise.resolve(fakeItems));
@@ -106,6 +136,6 @@ test("login and render list, add item, update item", async () => {
     });
 
     getAllByText(fakeItems[1].name);
-    expect(getAll).toBeCalledWith(user.uid);
+    expect(getAll).toBeCalledWith(mockedUser.uid);
     expect(getAll).toBeCalledTimes(1);
 });
